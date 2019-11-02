0badd0273dca3e558f77dec7d95b951d218cefb5
Jonathan Yim <canpake@users.noreply.github.com>
2019-11-02 16:34:01 +0000

    Drawing a path much more efficient

--- Game/rocket_test.py
+++ Game/rocket_test.py
@@ -1,3 +1,4 @@
+import math
 import random
 import arcade
 import os
@@ -7,8 +8,31 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "Rocket Test"
 SPRITE_SCALE_PLAYER = 0.05
-SPRITE_SCALE_PLANETS = 0.5
-SPRITE_SCALE_BOX = 1
+SPRITE_SCALE_PLANETS = 0.25
+SPRITE_SCALE_BOX = 0.2
+SPRITE_SCALE_PROJECTILE_DOT = 0.3
+PROJECTILE_DOT_FREQ = 10
+GRAV_CONST = 100
+
+DRAW_VELOCITY = False # warning: laggy
+DRAW_PATH = True
+
+
+# Finds gravitational acceleration between two objects, given their position and the larger object's mass
+def find_velocity(x1, y1, x2, y2, mass):
+
+    delta_x = x2 - x1
+    delta_y = y2 - y1
+    distance = math.sqrt(pow(delta_x, 2) + pow(delta_y, 2))
+
+    # g = GM / d^2, so calculate g
+    g = GRAV_CONST * mass / pow(distance, 2)
+
+    # Split g into x and y
+    gx = g * delta_x / distance
+    gy = g * delta_y / distance
+
+    return (gx, gy)
 
 
 class MyGame(arcade.Window):
@@ -30,12 +54,13 @@ class MyGame(arcade.Window):
         self.player_list = None
         self.planet_list = None
         self.projectile_list = None
+        self.path_list = None
 
         # Set up the player info
         self.player_sprite = None
-        #
-        # # Don't show the mouse cursor
-        # self.set_mouse_visible(False)
+
+        # Variable for drawing
+        self.draw_frame = 0
 
         arcade.set_background_color(arcade.color.AMAZON)
 
@@ -46,15 +71,18 @@ class MyGame(arcade.Window):
         self.player_list = arcade.SpriteList()
         self.planet_list = arcade.SpriteList()
         self.projectile_list = arcade.SpriteList()
+        self.path_list = arcade.ShapeElementList()
 
         # Set up planets
         planet1 = arcade.Sprite("images/planet_01.png", scale=SPRITE_SCALE_PLANETS)
         planet1.center_x = 600
         planet1.center_y = 300
+        planet1.collision_radius = 40
 
         planet2 = arcade.Sprite("images/planet_02.png", scale=SPRITE_SCALE_PLANETS)
         planet2.center_x = 200
         planet2.center_y = 300
+        planet2.collision_radius = 40
 
         self.planet_list.append(planet1)
         self.planet_list.append(planet2)
@@ -67,10 +95,27 @@ class MyGame(arcade.Window):
 
     def on_draw(self):
         """ Draw everything """
+        self.draw_frame += 1
+
         arcade.start_render()
         self.player_list.draw()
         self.planet_list.draw()
         self.projectile_list.draw()
+        self.path_list.draw()
+
+        if DRAW_PATH:
+            if self.draw_frame % PROJECTILE_DOT_FREQ == 0:
+                for projectile in self.projectile_list:
+                    path_dot = arcade.create_rectangle_filled(projectile.center_x, projectile.center_y, 10, 10, arcade.color.BLACK)
+                    self.path_list.append(path_dot)
+
+        # draw text on its velocity
+        if DRAW_VELOCITY:
+            for projectile in self.projectile_list:
+                velocity_x = f"velocity (x): {round(projectile.velocity[0], 2)}"
+                velocity_y = f"velocity (y): {round(projectile.velocity[1], 2)}"
+                arcade.draw_text(velocity_x, projectile.center_x + 10, projectile.center_y + 10, arcade.color.WHITE, 14)
+                arcade.draw_text(velocity_y, projectile.center_x + 10, projectile.center_y - 10, arcade.color.WHITE, 14)
 
     def on_mouse_motion(self, x, y, dx, dy):
         """ Handle Mouse Motion """
@@ -83,20 +128,43 @@ class MyGame(arcade.Window):
         projectile = arcade.Sprite("images/box.png", scale=SPRITE_SCALE_BOX)
         projectile.center_x = x
         projectile.center_y = y
-        projectile.velocity = (random.random(), random.random())
+        projectile.velocity = (3, 3)
 
         self.projectile_list.append(projectile)
+
+        # reset projectile path list upon click
+        self.path_list = arcade.ShapeElementList()
+
+    # def on_mouse_release(self, x: float, y: float, button: int,
+    #                      modifiers: int):
+
 
     def on_update(self, delta_time):
         """ Movement and game logic """
 
-        # Generate a list of all sprites that collided with the player.
-        planet_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.planet_list)
+        # Used to update a projectile's velocity
+        def update_projectile_velocity(x, y, velocity_x, velocity_y):
+            # Check its acceleration due to every planet and add it together
+            for planet in self.planet_list:
+                velocity_change = find_velocity(x, y, planet.center_x, planet.center_y, planet.collision_radius)
+                velocity_x += velocity_change[0]
+                velocity_y += velocity_change[1]
 
-        # Loop through each colliding sprite, remove it, and add to the score.
-        for planet in planet_hit_list:
-            planet.remove_from_sprite_lists()
+            return (velocity_x, velocity_y)
 
+        for projectile in self.projectile_list:
+            # update projectile depending on its velocity
+            projectile.update()
+            # change its velocity based on where the other planets are
+            projectile.velocity = update_projectile_velocity(projectile.center_x, projectile.center_y, projectile.velocity[0], projectile.velocity[1])
+
+        # Remove all projectiles that have collided with a planet
+        for planet in self.planet_list:
+            projectile_hit_list = arcade.check_for_collision_with_list(planet, self.projectile_list)
+
+            # Loop through each colliding projectile and remove it.
+            for projectile in projectile_hit_list:
+                projectile.remove_from_sprite_lists()
 
 
 
